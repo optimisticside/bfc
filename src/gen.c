225fbae41909b4013c950f8d59339732be9b8191
optimisticside <vijaysh312@gmail.com>
2021-11-23 01:30:41 +0000

    Fixed loop comparison

--- src/gen.c
+++ src/gen.c
@@ -94,7 +94,7 @@ void genloop(Node *parent, int *loopcnt, FILE *output) {
 	fprintf(output, "loop%d:\n", id);
 	genlist(parent->childs, loopcnt, output);
 
-	fprintf(output, "\tcmp 0, %%rax\n"
+	fprintf(output, "\tcmp $0, %%rax\n"
 			"\tjle loop%d\n", id);
 }
 
